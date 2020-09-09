39b07ec4d52ad00166ddb9e0665e7323a3b97677
Ryan McCarthy <ryanmccarthy031@gmail.com>
2020-09-09 10:08:03 -0400

    Attempt at an error page

---
+++ layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nuxt-error">
+    {{ message }}
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'nuxt-error',
+  layout: 'default', // optional
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data () {
+      return {
+          message: ''
+      }
+  },
+  computed: {
+    errorPage() {
+      if (this.error.statusCode === 404) {
+        return this.message="Oops! 404 Error."
+      }
+      // catch everything else
+      return this.message='Oh no. Something has gone wrong.';
+    },
+};
+</script>
