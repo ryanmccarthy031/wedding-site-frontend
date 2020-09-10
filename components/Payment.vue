2af86d0184e0f255dfa85b2e6237b4bd3dfc33c1
Ryan McCarthy <ryanmccarthy031@gmail.com>
2020-09-10 12:38:01 -0400

    Try to fix font source

--- components/Payment.vue
+++ components/Payment.vue
@@ -108,7 +108,7 @@ export default {
                 elements: {
                     fonts: [{
                         family: "IM Fell English",
-                        src: 'url("assets/fonts/IM_Fell_English")',
+                        src: 'url("~/assets/fonts/IM_Fell_English/IMFellEnglish-Regular.ttf")',
                         unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD',
                         display: 'swap',
                     }],
