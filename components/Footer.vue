4e59fe923462fc206a3d2c8f67c00343b56b5d77
Ryan McCarthy <ryanmccarthy031@gmail.com>
2020-09-11 11:44:58 -0400

    Add alt tag for footer image

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer>
     <b-row class="justify-content-center">
       <b-col cols="6" lg="3">
-        <b-img fluid src="/footer.png" />
+        <b-img alt="Decorative floral typographical ornament" fluid src="/footer.png" />
       </b-col>
     </b-row>
   </footer>
